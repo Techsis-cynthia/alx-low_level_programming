91892bd8ce13e995a836dcbc43e78d34634d06da
cynthia usunobun <cynthia.e.usunobun@gmail.com>
2023-03-16 15:06:54 -0700

    correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - A program that ends with the value 0
- * Return: 0(success)
+ * Return: 0 (success)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0
+	return (0):
 }
