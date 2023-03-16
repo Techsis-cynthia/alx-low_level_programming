2221323cf493c33b94be99157f6b96643b50c223
cynthia usunobun <cynthia.e.usunobun@gmail.com>
2023-03-16 14:29:22 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *  main - A program that ends with the value 0
+ *  Return: 0(success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0
+}
