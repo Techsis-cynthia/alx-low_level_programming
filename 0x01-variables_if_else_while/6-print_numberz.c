f2a833a3ed395b83dd085aa617ee9f1151f84713
cynthia usunobun <cynthia.e.usunobun@gmail.com>
2023-03-19 09:59:52 -0700

    print numberz 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,17 +5,17 @@
 /**
  * main - A program that prints all single digit numbers of base 10
  * starting from 0, followed by a new line
- * 
+ *
  * Return: Always 0 (success)
 */
 int main(void)
 
 {
 	int d;
-	
+
 	for (d = '0'; d <= '9'; d++)
 	putchar(d);
 	putchar('\n');
-	
+
 	return (0);
 }
