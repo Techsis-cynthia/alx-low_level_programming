2140e16a4021dafb485b8aadeb4876c701499e1b
cynthia usunobun <cynthia.e.usunobun@gmail.com>
2023-03-19 10:14:34 -0700

    smile in a mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - smile in a mirror
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low -)
+	putchar(low)
+	putchar('\n');
+
+	return (0);
+
+}
