caf0677b3a1a3436cc1b5c15a20ee0b06dca38f9
cynthia usunobun <cynthia.e.usunobun@gmail.com>
2023-05-02 06:23:23 -0700

    0print listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * print_listint: prints all the elements of a litint_t list
- * @h: linked list of type listint_t to print
+ * print_listint: prints all the elements of a listint_t list
+ * @h: head of the list
  *
  * Return: number of nodes
  */
