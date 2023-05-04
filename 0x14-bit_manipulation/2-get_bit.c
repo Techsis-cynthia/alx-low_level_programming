7d49254e00a400b02a350c3daa7fca94bb084ac2
cynthia usunobun <cynthia.e.usunobun@gmail.com>
2023-05-04 13:49:22 -0700

    2 get bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_bit - a function that returns the value of a bit at an index in a decimal number
+ * get_bit - returns the value of a bit at a given index
  * @n: the number to search
  * @index: the index of the bit
  *
